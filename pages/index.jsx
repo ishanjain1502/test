144774df39769a92c0eb70678ab57cbf2a362ce2
vatsal0601 <62330868+vatsal0601@users.noreply.github.com>
2022-10-19 21:37:10 +0530

    created border box

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,8 +2,16 @@ import styles from "../styles/Home.module.css";
 
 const Home = () => {
   return (
-    <main>
-      <h1>Hello, World</h1>
+    <main className={styles.main}>
+      <div className={styles.borderWrapper}>
+        <span className={styles.topLeft}></span>
+        <span className={styles.topRight}></span>
+        <span className={styles.bottomLeft}></span>
+        <span className={styles.bottomRight}></span>
+        <span className={styles.topLine}></span>
+        <span className={styles.bottomLine}></span>
+        <span className={styles.text}>Team No - 8</span>
+      </div>
     </main>
   );
 };
